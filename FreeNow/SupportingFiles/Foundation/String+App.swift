25e81ebaa8cdaee00d3a0aa5a55f84789b41af8c
Johann Werner <johann@wunder.org>
2019-09-19 10:26:37 +0200

    renamed files

--- FreeNow/SupportingFiles/Foundation/String+App.swift
+++ FreeNow/SupportingFiles/Foundation/String+App.swift
@@ -1,11 +1,3 @@
-//
-//  String+CarApp.swift
-//  CarApp
-//
-//  Created by Johann Werner on 16.08.19.
-//  Copyright © 2019 Johann Werner. All rights reserved.
-//
-
 import Foundation
 
 extension String {
